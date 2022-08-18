bf6adc49a15216b8462ee0e8ddf3431c49a48ee7
Luiz Paulo Franz <luizpaulofranz@gmail.com>
2022-08-17 22:37:34 -0300

    Adds a config file, to change configs on flavors.

--- lib/flavor_config.dart
+++ lib/flavor_config.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 enum Endpoints { items, details }
 
+/// This is the flavorized parameters that we will send to each flavor
+/// Here we would add a different base_url for dev, debug, release versions.
+
 class FlavorConfig {
   String appTitle;
   Map<Endpoints, String> apiEndpoint;
